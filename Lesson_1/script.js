bb85252ca096d371f3c7f645bee5649cd8815bab
Siarhei Sapelka <Sereban778@yandex.ru>
2020-01-05 23:29:38 +0300

    add budget

--- Lesson_1/script.js
+++ Lesson_1/script.js
@@ -16,11 +16,27 @@ for (let i = 0; i < 2; i++){
     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
         b = prompt("Во сколько обойдется?", '');
 
-    appData.expenses[a] = b;
+    if((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length <50) {
+        console.log('done');
+        appData.expenses[a] = b;
+    }
+};
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+    console.log("Низкий уровень бюджета");
+} else if(appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if(appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка");
 }
 
 
-alert(appData.budget / 30);
 
 
 

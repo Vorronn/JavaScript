354612eea9693c5c22b6d41b7303590873ca724e
Siarhei Sapelka <Sereban778@yandex.ru>
2020-01-07 01:10:22 +0300

    added three button (button - hide)

--- Lesson_2_1/JS/script.js
+++ Lesson_2_1/JS/script.js
@@ -8,10 +8,23 @@ btn.onclick = hello;
 let buttonShow = document.querySelector("#botton-show");
 console.log(buttonShow.id);
 
+
+
 function show() {
     let window = document.querySelector(".window");
     window.style.display = "block";
+    window.style.backgroundColor = "red";
 }
 
 buttonShow.onclick = show;
+
+let hide = document.querySelector("#hide");
+console.log(hide.id);
+
+function hiddenDiv() {
+    let window = document.querySelector(".window");
+    window.style.display ="none";
+}
+
+hide.onclick = hiddenDiv;
 

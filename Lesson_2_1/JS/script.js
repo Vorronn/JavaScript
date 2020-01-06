4fe3e2526211c0bdee8a8a9c56a97b00deaaeeec
Siarhei Sapelka <Sereban778@yandex.ru>
2020-01-06 22:03:19 +0300

    added two button (first button - 'Push me', second button - 'show'

--- Lesson_2_1/JS/script.js
+++ Lesson_2_1/JS/script.js
@@ -1,1 +1,17 @@
-alert("Hello world");
+function hello() {
+    console.log("Hello world!!!");
+}
+let btn = document.querySelector("#btn");
+console.log(btn.id);
+btn.onclick = hello;
+
+let buttonShow = document.querySelector("#botton-show");
+console.log(buttonShow.id);
+
+function show() {
+    let window = document.querySelector(".window");
+    window.style.display = "block";
+}
+
+buttonShow.onclick = show;
+

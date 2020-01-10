a1c63498319f971841556274c25fd54275e7b64d
Siarhei Sapelka <Sereban778@yandex.ru>
2020-01-10 22:18:12 +0300

    lesson on the main mouse events

--- Lesson_2_5/JS/script.js
+++ Lesson_2_5/JS/script.js
@@ -10,3 +10,22 @@ document.querySelector(".ondblclick").ondblclick = function() {
 document.querySelector(".onmousemove").onmousemove = function() {
     console.log("mousemove");
 };
+// заоход мышки на элемент один раз считает
+document.querySelector(".onmouseenter").onmouseenter = function() {
+    console.log("mouseenter");
+};
+// выход мышки из элемента
+document.querySelector(".onmouseleave").onmouseleave = function() {
+    console.log("mouseleave");
+};
+// прокрутка колесика мышки
+document.querySelector(".onmousewheel").onmousewheel = function() {
+    console.log("wheel");
+    return false;
+};
+
+// контект меню - не рекомендовано к использоавнию 
+document.querySelector(".oncontextmenu").oncontextmenu = function() {
+    console.log("contextmenu");
+    return false;
+};

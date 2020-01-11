8c5aa18d916e9bdb1b961a8d8f42e7907188e8af
Siarhei Sapelka <Sereban778@yandex.ru>
2020-01-12 00:39:50 +0300

    end of lesson: events and loops for a block of elements

--- Lesson_2_6/JS/script.js
+++ Lesson_2_6/JS/script.js
@@ -1,18 +1,32 @@
-var divElement = document.querySelectorAll(".block");
-console.log(divElement);
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Первый способ - работает с двумя вариантами получения списка элемента
+let blocks = document.querySelectorAll(".block");
+// или же этим метедом получения списка элементов
+// let blocks = document.getElementsByClassName("block");
 
-for (let i = 0; i < divElement.length; i++) {
-    divElement[i].onclick = function () {
-        console.log("OOO");
-        console.log(divElement[i].innerHTML);
-    };
-    
+// функция для использования в цикле или же forEach
+function clicks () {
+    // console.log("click");
+    // console.log(this);
+    // console.log(this.innerHTML);             // выводит текст блока с внутренними тегами
+    console.log(this.innerText);                // выводит текст блока без тегов
+    this.style.backgroundColor = "blue";        // меняет цвет блока
+    this.style.color = "white";                 // меняет цвет текста
+    this.style.borderRadius = "50%";            // меняет блок из квадрата в круг
+}
 
+// цикл для получения элемента из списка элементов
+for (let i = 0; i < blocks.length; i++) {
+    blocks[i].onclick = clicks;
 }
 
 
-// function elem () {
-//     for (let i = 0; i < divElement.length; i++) {
-//         console.log("block")
-//     }
-// }
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Второй способ
+// Работает только с let blocks = document.querySelectorAll(".block");
+
+// blocks.forEach(function (element) {
+//     // console.log(element);
+//     element.onclick = clicks;
+        
+// });

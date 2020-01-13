49db4b5ef2a31b3937c5e7c16360a59fe945db67
Siarhei Sapelka <Sereban778@yandex.ru>
2020-01-14 01:26:28 +0300

    Lesson on keyboard events

---
+++ Lesson_2_8/JS/script.js
@@ -0,0 +1,57 @@
+// пробная версия
+// document.querySelector("#test").onkeypress = function (e) {
+//     console.log(e.keyCode);
+//     if (e.keyCode < 48 || e.keyCode > 57) {
+//         console.log("Недопустимые символы");
+//     } else {
+//         this.value += e.key;
+//     }
+//     // e.altKey false
+//     // e.charCode 97
+//     // e.code "KeyA"
+//     // e.ctrKey false
+//     // e.key: "a"
+//     // e.keyCode: 97
+//     // e.shiftKey: false;
+//     return false;
+// }
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ограничение ввода символов в input только прописная латиница
+// document.querySelector("#test").onkeypress = function (e) {
+//     // console.log(e);
+//     // console.log(e.keyCode);
+//     if (e.keyCode < 97 || e.keyCode >122) {
+//         console.log("Неверный символ");
+//     } else {
+//         this.value +=e.key;
+//     }
+//     return false;
+// };
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// дважиение квадрата вправо влево
+// let left = 10;
+// document.onkeypress = function (e) {
+//     console.log(e.keyCode);
+//     let sq = document.querySelector(".sq");
+//     if (e.keyCode == 100) {
+//         left += 10;
+//         sq.style.marginLeft = left + "px";
+//     }
+//     if (e.keyCode == 97) {
+//         left -= 10;
+//         sq.style.marginLeft = left + "px";
+//     }
+// };
+
+document.onkeypress = function () {
+    console.log("keypress");
+};
+
+document.onkeydown = function () {
+    console.log("onkeydown");
+};
+
+document.onkeyup = function () {
+    console.log("onkeyup");
+};
+

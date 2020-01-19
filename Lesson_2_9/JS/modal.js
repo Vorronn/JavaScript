d66f6868cd659ae2c61073260436630b94a5ef35
Siarhei Sapelka <Sereban778@yandex.ru>
2020-01-19 22:08:38 +0300

    added the function of closing modal window

--- Lesson_2_9/JS/modal.js
+++ Lesson_2_9/JS/modal.js
@@ -1,4 +1,3 @@
-// console.log(modalShow);
 document.querySelectorAll(".modal-show").forEach(function (element) {
     element.onclick = showModal;
 });
@@ -6,7 +5,33 @@ document.querySelectorAll(".modal-show").forEach(function (element) {
 function showModal () {
     // console.log("yes");
     let modalId = this.dataset.modal;
-    console.log(modalId);
+    // console.log(modalId);
     document.querySelector(modalId).parentElement.classList.remove("hide");
     document.querySelector(modalId).classList.remove("hide");
+    document.onkeydown = function (e) {
+        console.log(e);
+        closeMenu();
+    };
+    
 }
+
+let buttonClose = document.querySelector(".button-close");
+buttonClose.onclick = closeMenu;
+
+let buttonExit = document.querySelector(".exit");
+buttonExit.onclick = closeMenu;
+
+let clicDivBlock = document.querySelector(".modal-wrap");
+clicDivBlock.onclick = closeMenu;
+
+function closeMenu () {
+    document.querySelector(".modal-wrap").classList.add("hide");
+    document.querySelector(".modal").classList.add("hide");
+    document.onkeydown = null;
+}
+
+let clickOk = document.querySelector(".button-ok");
+clickOk.onclick = function () {
+    closeMenu();
+    alert("new modal window");
+};

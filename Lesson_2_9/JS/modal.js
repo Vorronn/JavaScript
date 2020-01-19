b86fee69d27c5c8392b65f7ac78607d747c3627f
Siarhei Sapelka <Sereban778@yandex.ru>
2020-01-19 20:44:48 +0300

    add modal window

--- Lesson_2_9/JS/modal.js
+++ Lesson_2_9/JS/modal.js
@@ -6,7 +6,7 @@ document.querySelectorAll(".modal-show").forEach(function (element) {
 function showModal () {
     // console.log("yes");
     let modalId = this.dataset.modal;
-    
+    console.log(modalId);
     document.querySelector(modalId).parentElement.classList.remove("hide");
     document.querySelector(modalId).classList.remove("hide");
 }

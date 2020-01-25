bf7ce79bf82e0c0b2060dc9c4b841b947fc5aacf
Siarhei Sapelka <Sereban778@yandex.ru>
2020-01-25 20:37:53 +0300

    finish lesson_2_10

--- Lesson_2_10/JS/script.js
+++ Lesson_2_10/JS/script.js
@@ -1,3 +1,14 @@
-document.querySelector(".btn1").onclick = function () {
-    alert("hello");
+let formSlider = document.querySelector(".form-slider");
+document.querySelector(".read-rules").onclick = function(){
+    formSlider.style.marginLeft = "-300px";
+}
+
+let backForm = document.querySelectorAll(".back");
+// console.log(backForm);
+for (let i = 0; i < backForm.length; i++){
+    backForm[i].onclick = closeShow;
+}
+
+function closeShow () {
+    formSlider.style.marginLeft = "0";
 }

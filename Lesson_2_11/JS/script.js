774b6ea6a36598af55120eee0784acc4b7a7e8eb
Siarhei Sapelka <Sereban778@yandex.ru>
2020-01-26 00:06:34 +0300

    new lesson_2_11 form processing

---
+++ Lesson_2_11/JS/script.js
@@ -0,0 +1,14 @@
+let select = document.querySelector("#select-1");
+
+select.onchange = function () {
+    console.log(select.value);
+}
+
+let sendForm = document.querySelector("#send-form");
+let form = document.querySelector("form");
+
+sendForm.onclick = function (event) {
+    event.preventDefault();
+    console.log("work");
+    console.log(serialize(form));
+}

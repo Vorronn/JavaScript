28019bd98f10da55a1e0da4879b803adb57630bf
Siarhei Sapelka <Sereban778@yandex.ru>
2020-02-03 00:01:02 +0300

    disabling of floating elements to lesson_2_14

--- Lesson_2_14/js/test.js
+++ Lesson_2_14/js/test.js
@@ -1,1 +1,32 @@
-let click = document.querySelector(".click");
+// let click = document.querySelector(".click");
+// click.onclick = function () {
+//     console.log("click");
+// };
+
+function click (event) {
+    console.log("click!!!");
+    console.log("--------------------");
+    console.log(event.target);
+    console.log("--------------------");
+    console.log(this);
+}
+
+// document.querySelector(".click").onclick = click;
+// document.querySelector("body").onclick = click;
+// document.querySelector("html").onclick = click;
+
+document.onclick = function (event) {
+    // console.log(event.target.className);
+    if(event.target.className == "one"){
+        console.log("Клацнули по div");
+    }
+};
+
+document.querySelector(".three").onclick = function () {
+    event.stopPropagation(); // отключение всплытия окна
+    console.log("click three");
+};
+
+document.querySelector(".two").onclick = function () {
+    console.log("click two");
+};

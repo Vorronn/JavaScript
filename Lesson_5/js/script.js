297749e1445b186b5083418726d7682764a8ba11
Siarhei Sapelka <Sereban778@yandex.ru>
2020-03-14 23:10:29 +0300

    fixes file js for html Lesson 5

--- Lesson_5/js/script.js
+++ Lesson_5/js/script.js
@@ -1,7 +1,28 @@
-alert("hello");
-let listMenu = document.querySelectorAll(".menu-item");
-console.log(listMenu);
+let ul = document.querySelector(".menu"),
+    menu1 = document.querySelectorAll(".menu-item"),
+    remu = document.querySelectorAll(".column"),
+    del = document.querySelector(".adv"),
+    answer = document.querySelector(".prompt");
 
-parent.appenChild(li);
-console.log(listMenu);
 
+ul.removeChild(menu1[2]);
+ul.insertBefore(menu1[2], menu1[1]);
+    
+
+let li = document.createElement('li');
+li.classList.add('menu-item');
+ul.appendChild(li);
+li.insertAdjacentText("afterbegin","Пятый пункт");
+
+document.querySelector('body').style.background = 'url(img/apple_true.jpg) center no-repeat';
+document.querySelector(".title").innerHTML = "Мы продаем только подлинную технику Apple";
+console.log(remu);
+remu[1].removeChild(del);
+
+
+function quest(){
+let quiz = prompt("Как вы относитесь к технике Apple?");
+return quiz;    
+}
+
+answer.innerHTML = quest();

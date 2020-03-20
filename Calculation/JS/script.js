e467a13dc6b63ffefdd7e06da000f5b327da553a
Siarhei Sapelka <Sereban778@yandex.ru>
2020-03-20 10:00:33 +0300

    saving

--- Calculation/JS/script.js
+++ Calculation/JS/script.js
@@ -1,7 +1,7 @@
 let buttonInterpolation = document.querySelector(".buttonInterpolation");
 buttonInterpolation.onclick = showIntrepolation;
 
-function showIntrepolation(){
+function showIntrepolation() {
     let windowModal = document.querySelector(".modalWindow");
     windowModal.classList.remove("hide");
     document.querySelector(".interpolation").classList.remove("hide");
